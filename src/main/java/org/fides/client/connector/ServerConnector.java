e01d04a85949e285234ad94fb4a56fc046c3b1ea
Tom Kostense <tom.kostense@gmail.com>
2014-11-24 13:50:38 +0100

    FS-3 Truststore toegevoegd

--- src/main/java/org/fides/client/connector/ServerConnector.java
+++ src/main/java/org/fides/client/connector/ServerConnector.java
@@ -1,17 +1,56 @@
 package org.fides.client.connector;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.UnknownHostException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ServerConnector {
 
-	SSLSocket sslSocket;
+	SSLSocket sslsocket;
 
 	public ServerConnector() {
+		//For testing purposes only
+		Properties systemProps = System.getProperties();
+		systemProps.put( "javax.net.ssl.trustStore", "./truststore.ts");
+		systemProps.put("javax.net.ssl.trustStorePassword", "");
+		System.setProperties(systemProps);
 	}
 
 	public boolean connect() {
+		try {
+			SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+			//TODO Change serverip and port
+			sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", 4444);
+
+			SSLContext context = SSLContext.getInstance("TLS");
+
+			SSLSession session = sslsocket.getSession();
+			java.security.cert.Certificate[] servercerts = session.getPeerCertificates();
+
+
+
+			OutputStream outToServer = sslsocket.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outToServer);
+
+
+		} catch (UnknownHostException e) {
+			fail("UnknownHostException");
+		} catch (IOException e) {
+			fail("IOException");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
@@ -32,6 +71,7 @@ public class ServerConnector {
 	}
 
 	public boolean disconnect() {
+		sslsocket.close();
 		return false;
 	}
 
