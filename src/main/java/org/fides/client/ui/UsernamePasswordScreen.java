6af7a3c4aa2342da667e7154a7f3b0bfac82f34b
Jesse Kerkhoven <jesse.kerkhoven@gmail.com>
2014-11-27 15:40:54 +0100

    FS-27 moved hash function to own class

--- src/main/java/org/fides/client/ui/UsernamePasswordScreen.java
+++ src/main/java/org/fides/client/ui/UsernamePasswordScreen.java
@@ -7,8 +7,6 @@ import java.awt.event.HierarchyEvent;
 import java.awt.event.HierarchyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -19,7 +17,7 @@ import javax.swing.SwingUtilities;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bouncycastle.util.encoders.Base64;
+import org.fides.client.tools.Utils;
 
 /**
  * UI where a username and password can be submitted by a user
@@ -29,11 +27,6 @@ public class UsernamePasswordScreen {
 	 * Log for this class
 	 */
 	private static Logger log = LogManager.getLogger(UsernamePasswordScreen.class);
-
-	/**
-	 * hash function to hash password
-	 */
-	private static final String HASH = "SHA-256";
 
 	/**
 	 * Show a dialog where the user can enter its username and password
@@ -92,28 +85,14 @@ public class UsernamePasswordScreen {
 			String[] result = new String[3];
 			result[0] = "login";
 			result[1] = username.getText();
-			try {
-				MessageDigest messageDigest = MessageDigest.getInstance(HASH);
-				messageDigest.update(new String(password.getPassword()).getBytes());
-				result[2] = Base64.toBase64String(messageDigest.digest());
-			} catch (NoSuchAlgorithmException e) {
-				log.error(e);
-			}
+			result[2] = Utils.hash(new String(password.getPassword()));
 			return result;
 		} else if (option == 1) {
 			String[] result = new String[4];
 			result[0] = "register";
 			result[1] = username.getText();
-
-			try {
-				MessageDigest messageDigest = MessageDigest.getInstance(HASH);
-				messageDigest.update(new String(password.getPassword()).getBytes());
-				result[2] = Base64.toBase64String(messageDigest.digest());
-				messageDigest.update(new String(PasswordScreen.getPassword()).getBytes());
-				result[3] = Base64.toBase64String(messageDigest.digest());
-			} catch (NoSuchAlgorithmException e) {
-				log.error(e);
-			}
+			result[2] = Utils.hash(new String(password.getPassword()));
+			result[3] = Utils.hash(new String(PasswordScreen.getPassword()));
 			return result;
 
 		}
