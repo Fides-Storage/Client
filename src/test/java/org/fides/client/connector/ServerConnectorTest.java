cde5e98d4ec01f97bc10a1bec1adc7b57875075d
Koen Mulder <Noek1993@outlook.com>
2014-11-27 13:56:51 +0100

    FS-1 FS-2 Review comments

--- src/test/java/org/fides/client/connector/ServerConnectorTest.java
+++ src/test/java/org/fides/client/connector/ServerConnectorTest.java
@@ -1,16 +1,11 @@
 package org.fides.client.connector;
 
-import org.junit.Test;
-
-import java.net.UnknownHostException;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * The serverconnector unit test
  */
 public class ServerConnectorTest {
+
+	// TODO tests missing for the login and register
 
 }
