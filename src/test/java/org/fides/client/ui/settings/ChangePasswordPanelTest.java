909d9a1cdd074c6e9a8649c3e04c0a8d3633d40f
Tom Kostense <tom.kostense@gmail.com>
2015-01-06 10:54:54 +0100

    FS-7 Added testclass for passwordchange

---
+++ src/test/java/org/fides/client/ui/settings/ChangePasswordPanelTest.java
@@ -0,0 +1,30 @@
+package org.fides.client.ui.settings;
+
+import static org.junit.Assert.assertEquals;
+
+import org.fides.tools.HashUtils;
+import org.junit.Test;
+
+/**
+ * Test class for hash utilities
+ * 
+ */
+public class ChangePasswordPanelTest {
+
+	/**
+	 * Compares the expected hash with the actual hash to be equal
+	 */
+	@Test
+	public void testHashFunction() {
+		String hashTest = HashUtils.hash("This is a test string");
+		assertEquals("717ac506950da0ccb6404cdd5e7591f72018a20cbca27c8a423e9c9e5626ac61", hashTest);
+	}
+
+	/**
+	 * Expects a NullPointerException when null is given
+	 */
+	@Test(expected = NullPointerException.class)
+	public void testHashFunctionWithNullArgument() {
+		HashUtils.hash(null);
+	}
+}
